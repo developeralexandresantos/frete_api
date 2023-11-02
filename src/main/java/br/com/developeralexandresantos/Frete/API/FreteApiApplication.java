808429510e2d59cf674b59e2ac28b2378b0c2ba0
Alexandre Aparecido dos Santos <skeeter.182@uninove.edu.br>
2023-11-02 20:13:50 -0300

    first commit

---
+++ src/main/java/br/com/developeralexandresantos/Frete/API/FreteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.developeralexandresantos.Frete.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/developeralexandresantos/Frete/API/FreteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.developeralexandresantos.Frete.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
